87348a571dc508546a0ef777fa6633072f4ab206
ustc-liu <68310982+ustc-liu@users.noreply.github.com>
2023-12-12 15:49:53 +0800

    Delete genEigenMode.m

--- genEigenMode.m
+++
@@ -1,3 +0,0 @@
-function Psi = genEigenMode(n,l,r,phi)
-
-Psi = sqrt(factorial((n-abs(l))/2)/(pi*factorial((n+abs(l))/2)))*r.^(abs(l)).*laguerreL((n-abs(l))/2,abs(l),r.^2).*exp(-r.^2/2).*exp(1i*l*phi);
